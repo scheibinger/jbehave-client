e8aff6d315e03ff534f76422fb2d801e2e8a30bc
Radoslaw Scheibinger <rscheibinger@ebay.com>
2014-04-05 09:41:22 +0100

    Initial commit

---
+++ src/main/java/org/jbehave/client/Main.java
@@ -0,0 +1,10 @@
+package org.jbehave.client;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Running story "+ Arrays.toString(args));
+    }
+}
